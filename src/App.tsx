13cf35231badbfa92952cb2bedb6a21935a5e422
Morra <50861777+vlmorra@users.noreply.github.com>
2025-06-05 03:00:26 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { AuthKitProvider } from '@farcaster/auth-kit';
 import TamagotchiGame from './components/TamagotchiGame';
 import { farcasterConfig } from './farcasterConfig';
-import './App.css';
 
 export default function App() {
   return (
