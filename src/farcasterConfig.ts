6e6c801938c81e9d7b119cbb954a609d17e64c7e
Morra <50861777+vlmorra@users.noreply.github.com>
2025-06-07 07:28:31 +0800

    Delete src directory

--- src/farcasterConfig.ts
+++
@@ -1,6 +0,0 @@
-export const farcasterConfig = {
-  relay: "https://relay.farcaster.xyz",
-  rpcUrl: "https://optimism-mainnet.public.blastapi.io", // Для продакшена
-  domain: process.env.NEXT_PUBLIC_FARCASTER_DOMAIN, // Замените на ваш домен
-  siweUri: process.env.NEXT_PUBLIC_FARCASTER_DOMAIN_AUTH, // Важно для продакшена
-};
